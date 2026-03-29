899bb2ed6342a9e2ace03192803c991703855370
Priyanshu <pv254424@gmail.com>
2026-03-29 23:26:59 +0530

    serivce classes adding

--- auth-app/auth-app/src/main/java/com/substring/authentication/auth_app/services/UserService.java
+++ auth-app/auth-app/src/main/java/com/substring/authentication/auth_app/services/UserService.java
@@ -1,6 +1,19 @@
 package com.substring.authentication.auth_app.services;
 
+import com.substring.authentication.auth_app.payload.UserDto;
+
 public interface UserService {
 
-    
+    UserDto createUser(UserDto userDto);
+
+    UserDto getUserByEmail(String email);
+
+    UserDto updateUser(UserDto userDto, String userId);
+
+    void deleteUser(String userId);
+
+    UserDto getUserById(String userId);
+
+    Iterable<UserDto> getAllUsers();
+
 }
