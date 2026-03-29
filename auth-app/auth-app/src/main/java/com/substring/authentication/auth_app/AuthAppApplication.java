d7d2332067c4c6d6df09b2bca88b8120f082477f
Priyanshu <pv254424@gmail.com>
2026-03-29 12:27:25 +0530

    first commit

---
+++ auth-app/auth-app/src/main/java/com/substring/authentication/auth_app/AuthAppApplication.java
@@ -0,0 +1,13 @@
+package com.substring.authentication.auth_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthAppApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(AuthAppApplication.class, args);
+	}
+
+}
